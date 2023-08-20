aa6d43bec10e9e7056d8d806b0ade937dc858d32
taibasultana <tsmtaibasultana@gmail.com>
2023-08-20 22:16:50 +0600

    up

---
+++ Function-Object/function.js
@@ -0,0 +1,15 @@
+// function declaration
+function startFan(){
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+}
+
+// call the funtion
+startFan();
+console.log('Waking in the morning')
+startFan();
+console.log('Eating lunch')
+startFan();
+console.log('Watching JS tutorial')
+startFan();
---
+++ Function-Object/parameter.js
@@ -0,0 +1,22 @@
+function bringSingara(money){
+    console.log('taka disen:', money);
+    console.log('ei nen singara');
+    
+}
+var taka = 300;
+
+// bringSingara(300);
+
+function add(num1, num2){
+    console.log('going to add:',num1, num2);
+}
+
+// add(125, 96);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(15, 98, 56, 5, 9);
