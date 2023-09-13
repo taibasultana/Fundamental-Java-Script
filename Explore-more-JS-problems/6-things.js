0fca2690f5e564399916637c64b0502d7281382e
taibasultana <tsmtaibasultana@gmail.com>
2023-09-13 11:14:10 +0600

    up

---
+++ Explore-more-JS-problems/6-things.js
@@ -0,0 +1,12 @@
+// 1. Variable: Variables are containers for storing information. Creating a variable in JavaScript is called "declaring" a variable: var carName;
+
+// 2. Conditional:Conditionals are expressions that evaluate to either true or false. They are mostly used to determine Program Flow through if statements and while loops.
+
+// 3. Array: Arrays are used when we have a list of items. An array is an object that can store multiple values at once. For example, const words = ['hello', 'world', 'welcome']; Here, words is an array.
+
+// 4. Loop:  to perform repeated tasks based on a condition
+
+// 5. functio: A function is declared using the function keyword
+
+// 6. Object: The Object type represents one of JavaScript's data types. 
+    
